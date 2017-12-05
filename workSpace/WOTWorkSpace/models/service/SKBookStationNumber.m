fd4a39440215ba49784acd3fb9be52f74b0f59b8
wangxiaodong-ios <wangxiaodong@csii993.local>
2017-12-05 18:43:35 +0800

    修改查询所有空间接口

---
+++ workSpace/WOTWorkSpace/models/service/SKBookStationNumber.m
@@ -0,0 +1,13 @@
+//
+//  SKBookStationNumber.m
+//  WOTWorkSpace
+//
+//  Created by wangxiaodong on 2017/12/5.
+//  Copyright © 2017年 北京物联港科技发展有限公司. All rights reserved.
+//
+
+#import "SKBookStationNumber.h"
+
+@implementation SKBookStationNumber
+
+@end
---
+++ workSpace/WOTWorkSpace/models/service/SKBookStationNumberModel.h
@@ -0,0 +1,13 @@
+//
+//  SKBookStationNumberModel.h
+//  WOTWorkSpace
+//
+//  Created by wangxiaodong on 2017/12/5.
+//  Copyright © 2017年 北京物联港科技发展有限公司. All rights reserved.
+//
+
+#import <JSONModel/JSONModel.h>
+
+@interface SKBookStationNumberModel : JSONModel
+
+@end
